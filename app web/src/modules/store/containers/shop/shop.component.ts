f62f33854b1da0d7576593d7917bbd6fd9fc66f1
gabriel conejo <gacova95@gmail.com>
2021-05-15 22:16:10 -0600

    espacio para los scripts sql
    faltante de la tienda

--- app web/src/modules/store/containers/shop/shop.component.ts
+++ app web/src/modules/store/containers/shop/shop.component.ts
@@ -20,28 +20,28 @@ export class ShopComponent implements OnInit {
         {
           title: 'Card Title 1',
           description: 'Some quick example text to build on the card title and make up the bulk of the card content',
-          buttonText: 'Button',
+          precio: 30000,
           img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg',
           stars:5
         },
         {
           title: 'Card Title 2',
           description: 'Some quick example text to build on the card title and make up the bulk of the card content',
-          buttonText: 'Button',
+          precio: 30000,
           img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg',
           stars:4
         },
         {
           title: 'Card Title 3',
           description: 'Some quick example text to build on the card title and make up the bulk of the card content',
-          buttonText: 'Button',
+          precio: 30000,
           img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg',
           stars:3
         },
         {
           title: 'Card Title 4',
           description: 'Some quick example text to build on the card title and make up the bulk of the card content',
-          buttonText: 'Button',
+          precio: 30000,
           img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg',
           stars:2
         },
