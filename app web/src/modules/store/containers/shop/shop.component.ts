5ae97b0af9390ee2ca57118def74195bc31e70eb
gabriel conejo <gacova95@gmail.com>
2021-05-15 19:26:07 -0600

    agregado de cards para tienda

--- app web/src/modules/store/containers/shop/shop.component.ts
+++ app web/src/modules/store/containers/shop/shop.component.ts
@@ -10,9 +10,9 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
     styleUrls: ['shop.component.scss'],
 })
 export class ShopComponent implements OnInit {
-    ImagePath: string;
+    Carrus: string;
     constructor() {
-        this.ImagePath = '/assets/img/f.png'
+        this.Carrus = '/assets/img/f.png'
 
     }
     ngOnInit() {}
@@ -21,26 +21,34 @@ export class ShopComponent implements OnInit {
           title: 'Card Title 1',
           description: 'Some quick example text to build on the card title and make up the bulk of the card content',
           buttonText: 'Button',
-          img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
+          img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg',
+          stars:5
         },
         {
           title: 'Card Title 2',
           description: 'Some quick example text to build on the card title and make up the bulk of the card content',
           buttonText: 'Button',
-          img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
+          img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg',
+          stars:4
         },
         {
           title: 'Card Title 3',
           description: 'Some quick example text to build on the card title and make up the bulk of the card content',
           buttonText: 'Button',
-          img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
+          img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg',
+          stars:3
         },
         {
           title: 'Card Title 4',
           description: 'Some quick example text to build on the card title and make up the bulk of the card content',
           buttonText: 'Button',
-          img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
+          img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg',
+          stars:2
         },
     
       ];
+
+      counter(i: number) {
+        return new Array(i);
+    }
 }
